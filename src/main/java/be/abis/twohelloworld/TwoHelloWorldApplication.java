41b6e166397c786663dde2f7dae69b6a96300967
alfu32 <ioan.alferaru@gmail.com>
2024-10-22 09:27:35 +0200

    feat(first):

---
+++ src/main/java/be/abis/twohelloworld/TwoHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package be.abis.twohelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwoHelloWorldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/abis/twohelloworld/TwoHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package be.abis.twohelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoHelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
