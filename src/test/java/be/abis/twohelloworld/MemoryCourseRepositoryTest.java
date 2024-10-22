baa036c77c6f223e6bbe7dfaeac0ee18aae2781a
alferarui <alferaru.ioan@gmail.com>
2024-10-22 14:01:41 +0200

    feat(mem-repo):

---
+++ src/test/java/be/abis/twohelloworld/MemoryCourseRepositoryTest.java
@@ -0,0 +1,49 @@
+package be.abis.twohelloworld;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemoryCourseRepositoryTest {
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findAllCoursesTest() {
+
+    }
+
+    @Test
+    void findCourseByIdTest() {
+
+    }
+
+    @Test
+    void findCourseByShortTitleTest() {
+
+    }
+
+    @Test
+    void addCourseTest() {
+
+    }
+
+    @Test
+    void updateCourseTest() {
+
+    }
+
+    @Test
+    void deleteCourseTest() {
+
+    }
+}
