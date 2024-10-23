3287f34f6f8ff68b445144e51ceec3dcdc44b563
alfu32 <ioan.alferaru@gmail.com>
2024-10-23 14:22:11 +0200

    feat(utilities):

---
+++ src/main/java/be/abis/twohelloworld/utilities/MyUtillity.java
@@ -0,0 +1,9 @@
+package be.abis.twohelloworld.utilities;
+
+import be.abis.twohelloworld.model.Course;
+
+public class MyUtillity {
+    public static void printCourse(Course course) {
+        System.out.println(course);
+    }
+}
