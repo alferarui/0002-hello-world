54a6b865817176551729f1e355ea0d4df03eb68d
alferarui <alferaru.ioan@gmail.com>
2024-10-25 15:35:39 +0200

    feat(jdbc): #5 jdbc storage
    configured and tested sqlite and oracle connections
    added datasets/migeration

--- src/test/java/be/abis/twohelloworld/configuration/DataSourceTest.java
+++ src/test/java/be/abis/twohelloworld/configuration/DataSourceTest.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
